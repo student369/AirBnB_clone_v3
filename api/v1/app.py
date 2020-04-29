49c06dafd4f389ec3690df49b9e9bd3af92632df
Jose Calderon <jose.calderon@holbertonschool.com>
2020-04-29 17:45:43 -0500

    Adding the app.py file

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,32 +1,6 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-""" app api """
-from flask import Flask, jsonify, Blueprint, make_response
-from models import storage
-from api.v1.views import app_views
-import os
-app = Flask(__name__)
-app.register_blueprint(app_views)
-
-
-@app.teardown_appcontext
-def teardown_appcontext(self):
-    """call to close() method """
-    storage.close()
-
-
-@app.errorhandler(404)
-def erro_404(error):
-    """ Err 404 """
-    return make_response(jsonify({'error': 'Not found'}), 404)
-
-if __name__ == "__main__":
-    host = os.getenv("HBNB_API_HOST", '0.0.0.0')
-    port = os.getenv("HBNB_API_PORT", 5000)
-    app.run(host, port, threaded=True)
-=======
 """ Modules """
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import environ
@@ -41,6 +15,12 @@ def teardown_appcontext(error):
     storage.close()
 
 
+@app.errorhandler(404)
+def erro_404(error):
+    """ Err 404 """
+    return make_response(jsonify({'error': 'Not found'}), 404)
+
+
 if __name__ == "__main__":
     host_d = environ.get('HBNB_API_HOST')
     port_d = environ.get('HBNB_API_PORT')
@@ -50,4 +30,3 @@ if __name__ == "__main__":
     if (not port_d):
         port_d = '5000'
     app.run(host=host_d, port=port_d, threaded=True)
->>>>>>> 53b4c3511c83a85299b5ec04fd6ab247850ea70b
