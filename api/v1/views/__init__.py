67c3a3cc5e3bcf6d88d1685d1d0188fd05d2941a
Jose Calderon <jose.calderon@holbertonschool.com>
2020-04-29 19:24:05 -0500

    Adding the __init__.py file.

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,12 +1,6 @@
 #!/usr/bin/python3
-<<<<<<< HEAD
-from flask import Blueprint
-app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-=======
 """ Module Blueprint"""
 from flask import Blueprint
 
 app_views = Blueprint('app_views', __name__, url_prefix='/api/v1')
-
->>>>>>> 53b4c3511c83a85299b5ec04fd6ab247850ea70b
 from api.v1.views.index import *
